a4a4879b4b23faa7303ab7f18321ed6255c00531
Suyash <suyash.saraf@somaiya.edu>
2023-09-09 09:10:19 +0530

    test

--- Theory.java
+++ Theory.java
@@ -8,7 +8,7 @@ class Theory {
                 "Explain method overloading in java"
         };
     //Will've change functionality in evaluate_ans so that if 85-90% characters in keywords matches it will evaluate to true
-	//in case user enter different grammatical form of same sentence or makes some dumb spelling mistake. 
+	//in case user enter different grammatical form of same word or makes some dumb spelling mistake. 
         String[] keys = {
                 "memory,address,location,reference,refrence,&,*",
                 "classes,objects,class,oject,abstraction,encapsulation,inheritance,polymorphism,access,specifiers,public,private,protected,interface,interfaces",
